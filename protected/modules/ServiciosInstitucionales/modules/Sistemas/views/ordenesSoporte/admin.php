95af468524e5bf484a875e4af1b44ffc318717a7
Mitzi Moncada <nesmones1591@hotmail.com>
2014-07-02 10:28:15 -0500

    sistemasoperador sólo puede crear incidencias, no verlas

--- protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/admin.php
+++ protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/admin.php
@@ -9,12 +9,24 @@ $this->breadcrumbs=array(
 
 
 <?php
+if(Yii::app()->user->checkAccess('SistemaCapturista')) {
 $this->widget(
 	'bootstrap.widgets.TbTabs', array(
 		'type'=>'tabs', // 'tabs' or 'pills'
 		'htmlOptions'=>array('overflow'=>'hidden'),
 		'tabs'=>array(
-			array(
+			
+
+			array('label'=>'Pendientes', 'content' => $this->renderPartial('adminListPendientes',
+				array('model' => $modelPendientes,),true),'active'=>true),//tab 2
+			array('label'=>'En proceso', 'content' => $this->renderPartial('adminListEnProceso',
+				array('model' => $modelEnProceso,),true),),//tab 3
+			array('label'=>'Terminadas', 'content' => $this->renderPartial('adminListTerminadas',
+				array('model' => $modelTeminadas,),true),),//tab 4
+			array('label'=>'Reportes','htmlOptions'=>array('overflow'=>'hidden'), 'content' => $this->renderPartial('reportes',
+				array('model' => $modelTeminadas,),true),),//tab 5
+				
+				array(
 				'label'=>'Crear nueva',
 				'content'=>
 				$this->renderPartial(
@@ -22,18 +34,20 @@ $this->widget(
 						'model' => $model,
 					),true
 				),//render partial
-				'active'=>true
 			),//tab 1
-
-			array('label'=>'Pendientes', 'content' => $this->renderPartial('adminListPendientes',
-				array('model' => $modelPendientes,),true),),//tab 2
-			array('label'=>'En proceso', 'content' => $this->renderPartial('adminListEnProceso',
-				array('model' => $modelEnProceso,),true),),//tab 3
-			array('label'=>'Terminadas', 'content' => $this->renderPartial('adminListTerminadas',
-				array('model' => $modelTeminadas,),true),),//tab 4
-			array('label'=>'Reportes','htmlOptions'=>array('overflow'=>'hidden'), 'content' => $this->renderPartial('reportes',
-				array('model' => $modelTeminadas,),true),),//tab 5
 			/**/
 		),//tabs
-));?>
+));
+
+}
+else{
+?> 
+<br/>
+<?php
+	$this->renderPartial(
+					'_formmin', array(
+						'model' => $model,
+					));
+}
+?>
 
