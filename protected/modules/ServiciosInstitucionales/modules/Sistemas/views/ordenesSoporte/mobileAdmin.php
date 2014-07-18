1efad630d1e3f8ff7a003b7bab6999cfb060b85d
Mitzi Moncada <nesmones1591@hotmail.com>
2014-07-18 11:29:31 -0500

    prueba de la cámara

--- protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/mobileAdmin.php
+++ protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/mobileAdmin.php
@@ -13,6 +13,69 @@ $this->breadcrumbs=array(
 );
 ?>
 <br/><br/>
+
+
+
+<video id="video" width="640" height="480" autoplay></video>
+<button id="snap">Snap Photo</button>
+<canvas id="canvas" width="640" height="480"></canvas>
+
+
+<script>
+
+		// Put event listeners into place
+		window.addEventListener("DOMContentLoaded", function() {
+			// Grab elements, create settings, etc.
+			var canvas = document.getElementById("canvas"),
+				context = canvas.getContext("2d"),
+				video = document.getElementById("video"),
+				videoObj = { "video": true },
+				errBack = function(error) {
+					console.log("Video capture error: ", error.code); 
+				};
+
+			// Put video listeners into place
+			if(navigator.getUserMedia) { // Standard
+				navigator.getUserMedia(videoObj, function(stream) {
+					video.src = stream;
+					video.play();
+				}, errBack);
+			} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+				navigator.webkitGetUserMedia(videoObj, function(stream){
+					video.src = window.webkitURL.createObjectURL(stream);
+					video.play();
+				}, errBack);
+			} else if(navigator.mozGetUserMedia) { // WebKit-prefixed
+				navigator.mozGetUserMedia(videoObj, function(stream){
+					video.src = window.URL.createObjectURL(stream);
+					video.play();
+				}, errBack);
+			}
+
+			// Trigger photo take
+			document.getElementById("snap").addEventListener("click", function() {
+				context.drawImage(video, 0, 0, 640, 480);
+			});
+		}, false);
+
+	</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<br/><br/>
 <?php
 	foreach(Yii::app()->user->getFlashes() as $key => $message) {
 		echo '<div class="alert alert-warning" role="alert">' . $message . '</div>';
