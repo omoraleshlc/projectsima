fc58d09d75da5a1224543c9e40912712cea4a853
Mitzi Moncada <nesmones1591@hotmail.com>
2014-07-18 11:53:35 -0500

    trying againg the camera

--- protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/mobileAdmin.php
+++ protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/mobileAdmin.php
@@ -16,49 +16,9 @@ $this->breadcrumbs=array(
 
 
 
-<video id="video" width="640" height="480" autoplay></video>
-<button id="snap">Snap Photo</button>
-<canvas id="canvas" width="640" height="480"></canvas>
 
+<input type="file" accept="image/*" capture="camera">
 
-<script>
-
-		// Put event listeners into place
-		window.addEventListener("DOMContentLoaded", function() {
-			// Grab elements, create settings, etc.
-			var canvas = document.getElementById("canvas"),
-				context = canvas.getContext("2d"),
-				video = document.getElementById("video"),
-				videoObj = { "video": true },
-				errBack = function(error) {
-					console.log("Video capture error: ", error.code); 
-				};
-
-			// Put video listeners into place
-			if(navigator.getUserMedia) { // Standard
-				navigator.getUserMedia(videoObj, function(stream) {
-					video.src = stream;
-					video.play();
-				}, errBack);
-			} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-				navigator.webkitGetUserMedia(videoObj, function(stream){
-					video.src = window.webkitURL.createObjectURL(stream);
-					video.play();
-				}, errBack);
-			} else if(navigator.mozGetUserMedia) { // WebKit-prefixed
-				navigator.mozGetUserMedia(videoObj, function(stream){
-					video.src = window.URL.createObjectURL(stream);
-					video.play();
-				}, errBack);
-			}
-
-			// Trigger photo take
-			document.getElementById("snap").addEventListener("click", function() {
-				context.drawImage(video, 0, 0, 640, 480);
-			});
-		}, false);
-
-	</script>
 
 
 
