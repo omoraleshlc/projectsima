f69c0301503143045f31e49f8d0c55868068b1f9
Mitzi Moncada <nesmones1591@hotmail.com>
2014-08-14 12:08:21 -0500

    observaciones en escaneo

--- protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/scanLabels.php
+++ protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/scanLabels.php
@@ -59,16 +59,37 @@
 	*/
 	#no-more-tables td:before { content: attr(data-title); }
 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
 </style>
+	
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Agregar observaciones</h4>
+      </div>
+      <div class="modal-body">
+      	<iframe style="width:100%; height:0px"
+      	src="" id="idorden">
+			</iframe>
+      </div>
+    </div>
+  </div>
+</div>
+
+	
+	
+	
+	
+	
+	
+	
+	
 <?php
 /* @var $this OrdenesSoporteController */
 /* @var $model OrdenesSoporte */
