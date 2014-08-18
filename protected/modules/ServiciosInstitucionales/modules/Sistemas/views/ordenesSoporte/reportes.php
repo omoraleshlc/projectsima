c72c22471205f3cda9d1c925f7b5d506b9becbc0
Mitzi Moncada <nesmones1591@hotmail.com>
2014-08-18 14:48:54 -0500

    se puede limitar por fecha el rango de ordenes que se toman para reportes

--- protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/reportes.php
+++ protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/reportes.php
@@ -1,13 +1,68 @@
+Ver reportes desde: 
+<form id="ordenes-soporte-form" action="/projectsima/index.php?r=ServiciosInstitucionales/Sistemas/ordenesSoporte/admin" method="post">
+		<?php 
+			Yii::import('application.extensions.CJuiDateTimePicker.CJuiDateTimePicker');
+		 	$this->widget('CJuiDateTimePicker',array(
+				'model'=>ordenesSoporte::model(), //Model object
+				'attribute'=>'fecha', //attribute name
+				'mode'=>'date', //use "time","date" or "datetime" (default)
+				'options'=>array('timeFormat'=>'hh:mm:ss',
+        			'dateFormat' => 'yy-mm-dd') // jquery plugin options
+			));
+		?>
+
+hasta:		
+				<?php 
+			Yii::import('application.extensions.CJuiDateTimePicker.CJuiDateTimePicker');
+		 	$this->widget('CJuiDateTimePicker',array(
+				'model'=>ordenesSoporte::model(), //Model object
+				'attribute'=>'fechaFinal', //attribute name
+				'mode'=>'date', //use "time","date" or "datetime" (default)
+				'options'=>array('timeFormat'=>'hh:mm',
+        			'dateFormat' => 'yy-mm-dd') // jquery plugin options
+			));
+			?>
+
+				<?php 
+			echo CHtml::submitButton('Generar reporte');
+		?>
+		</form>
+<br/>
+
 <?php $this->widget('bootstrap.widgets.TbButton', array(
     'label'=>'Imprimir ordenes',
     'url'=>'index.php?r=ServiciosInstitucionales/Sistemas/OrdenesSoporte/listadoParaImprimir',
 )); ?>
+
+
+
+<?php
+	if (isset($_POST['OrdenesSoporte'])){
+		$orden=$_POST['OrdenesSoporte'];
+		$duplicateData=Yii::app()->db->createCommand('
+		 Select almacen, COUNT(*) as num from  sima.sis_ordenesSOP where almacenSoporte="HSIST" and fecha between "'.$orden['fecha'].'" and "'.$orden['fechaFinal'].'" group by almacen order by num
+	')->queryAll();
+	
+		$labelFecha=$orden['fecha'].' a '.$orden['fechaFinal'];
+	}
+	else{
+		$duplicateData=Yii::app()->db->createCommand('
+			 Select almacen, COUNT(*) as num from  sima.sis_ordenesSOP where almacenSoporte="HSIST" group by almacen order by num
+		')->queryAll();
+		
+		$labelFecha='todos los tiempos.';
+	}
+?>
+<br/>
+<h3>Reporte de <?php echo $labelFecha;?></h3>
+
+
+
 <?php
 	Yii::import('application.extensions.Hzl.google.HzlVisualizationChart');
-  
-	$duplicateData=Yii::app()->db->createCommand('
-		 Select almacen, COUNT(*) as num from  sima.sis_ordenesSOP where almacenSoporte="HSIST" group by almacen order by num
-	')->queryAll();
+	
+	
+
 
 
 	$stack = array(array("", "Almacen"));	
