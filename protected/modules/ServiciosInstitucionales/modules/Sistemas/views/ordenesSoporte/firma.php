dd1d805e8751ae27ae4f487f38d6d85078ccbf80
Mitzi Moncada <nesmones1591@hotmail.com>
2014-08-22 12:56:04 -0500

    firmas se guardan en un archivo en /protected/firmas/

--- protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/firma.php
+++ protected/modules/ServiciosInstitucionales/modules/Sistemas/views/ordenesSoporte/firma.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'signature-to-image.php';
 /* @var $this OrdenesSoporteController */
 /* @var $data OrdenesSoporte */
 $baseUrl = Yii::app()->baseUrl; 
@@ -8,14 +9,39 @@ $cs->registerScriptFile($baseUrl.'/js/json2.min.js');
 $cs->registerCssFile($baseUrl.'/css/jquery.signaturepad.css');
 
 ?>
+
+<?php
+/* @var $this DefaultController */
+
+if(isset($_POST['output']) ){
+	$json = $_POST['output'];
+	$img = sigJsonToImage($json, array('imageSize'=>array(930, 100)));
+	imagepng($img, 'protected/firmas/signature.png');
+	imagedestroy($img);/**/
+}
+
+
+Yii::app()->clientScript->registerScript('comprimir', "
+		$(document).ready(function () {
+				var options = {
+					defaultAction : 'drawIt',
+					lineTop : '70',
+					errorMessageDraw: 'Firme, por favor.',
+					errorMessage: 'Formulario inválido'	
+				};
+				$('.sigPad').signaturePad(options);
+			
+		});
+		
+	");
+?>
+
 <script>
-$(document).ready(function () {
-  $('.sigPad').signaturePad();
-});
+
 </script>
 
 <div style="margin: auto; width: 80%">
-<form method="post" action="" class="sigPad">
+<form method="post" action="" class="sigPad" id="signature_form">
   <label for="name">Escribe tu nombre</label>
   <input type="text" name="name" id="name" class="name">
   <p class="typeItDesc">Revisa tu firma</p>
@@ -28,11 +54,9 @@ $(document).ready(function () {
   <div class="sig sigWrapper">
     <div class="typed"></div>
     <canvas class="pad" width="930" height="100"></canvas>
-    <input type="hidden" name="output" class="output">
+    <input type="hidden" id="output" name="output" class="output" onchange="alert('hi')">
   </div>
   <button type="submit">Acepto el trabajo realizado de esta orden de soporte.</button>
 </form>
 	
-<?php 
-	?>
 </div>
