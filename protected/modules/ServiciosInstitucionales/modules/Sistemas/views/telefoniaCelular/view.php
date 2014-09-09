6760d097bcf6bed205f368275c3da02af3ae40c3
Mitzi Moncada <nesmones1591@hotmail.com>
2014-09-09 18:15:21 -0500

    formato más detallado y con qr en la esquina superior

--- protected/modules/ServiciosInstitucionales/modules/Sistemas/views/telefoniaCelular/view.php
+++ protected/modules/ServiciosInstitucionales/modules/Sistemas/views/telefoniaCelular/view.php
@@ -29,14 +29,16 @@ echo CHtml::image(Yii::app()->request->baseUrl.'/images/hlclogo2.png');
 
 <div>
 	<h1>Ver Equipo de Telefonía #<?php echo $model->keyCTC; ?></h1>
-	<h2><?php echo CHtml::encode($model->getAttributeLabel('codigo')); ?>:
-		<?php echo CHtml::link(CHtml::encode($model->codigo), array('view', 'id'=>$model->keyCTC)); ?></h2>
-	<h3>Equipo para <?php  echo $model->usuarioCelular; ?> dado de alta por <?php  echo $model->usuario; ?></h3>
+	<h3><?php echo CHtml::encode($model->getAttributeLabel('codigo')); ?>:
+		<?php echo CHtml::link(CHtml::encode($model->codigo), array('view', 'id'=>$model->keyCTC)); ?></h3>
+	<div style="width:50%; float: left;">
+	<br/><br/>
+	<b>Equipo para <?php  echo $model->usuarioCelular; ?> dado de alta por <?php  echo $model->usuario; ?></b>
 	
 	<div>
-				<?php  echo $model->company?$model2->find('keyTS="' . $model->company . '"')->descripcion:'Marca no especificada'; ?> modelo <?php echo $model->modelo?$model->modelo:' no especificado'; ?><?php  echo $model->keyP?' proveido por '.$model5->find('keyP="' . $model->keyP . '"')->razonSocial:' '; ?>
-			<br/>
 			Número telefónico <?php  echo $model->nTelefonico; ?>
+			<br/>
+				<?php  echo $model->company?$model2->find('keyTS="' . $model->company . '"')->descripcion:'Marca no especificada'; ?> modelo <?php echo $model->modelo?$model->modelo:' no especificado'; ?><?php  echo $model->keyP?' proveido por '.$model5->find('keyP="' . $model->keyP . '"')->razonSocial:' '; ?>
 	</div>
 	
 	<div>
@@ -44,44 +46,9 @@ echo CHtml::image(Yii::app()->request->baseUrl.'/images/hlclogo2.png');
 			<br/>
 			<?php  echo $model->descripcionUbicacion; ?>
 	</div>
-
-		<br />
-	
-	
-
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'chip',
-		'imei',
-		'plan',
-		'precioEquipo',
-		'costoRenta',
-		'minutos',
-		'sms',
-		'internet',
-		'fechaContratacion',
-		'fechaInicial',
-		'fechaFinal',
-	),
-)); ?>
-</div>
-<br/>
-<div class="visible-print-block" style="padding-top: 80px; padding-bottom: 30px; width: 100%">
-	<div style="width: 30%; border-top: solid black 1px; display:inline; float: right;">
-		Firma de entregado
-		<br/>
 	</div>
-	<div style="width: 30%; border-top: solid black 1px; diplay: inline; float: left;">
-		Firma de recibido
-		<br/>
-	</div>
-	
-</div>
-<br/>
-
-<div class="view tetris-thumbnail <?php echo !isset($model->codigo)?'hidden-print':'' ?>">
-	<div class="graybox" style="border: solid 1px LightGray;">
+	<div  style="width:30%; float: right;" class="view tetris-thumbnail <?php echo !isset($model->codigo)?'hidden-print':'' ?>">
+	<div class="graybox show" style="border: solid 1px LightGray;">
 		<span class="visible-print-block" style="text-align:center;"><b>Hospital La Carlota</b></span>
 		<span class="visible-print-block" style="text-align:center; zoom:0.7">Código de equipo</span><br/>
 		<?php if(isset($model->codigo) && $model->codigo!="") {
@@ -98,6 +65,128 @@ echo CHtml::image(Yii::app()->request->baseUrl.'/images/hlclogo2.png');
 		<b><?php echo CHtml::link(CHtml::encode($model->codigo),array('telefoniaCelular/update','id'=>$model->keyCTC), array('style'=>'zoom:1.3;')); ?></b>
 	</div>
 </div>
+	
+	
+	
+
+		<br />
+		
+		<table style="font-size: 0.9em">
+			<tbody>
+				<tr>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('chip')); ?>:</b>
+						<?php echo CHtml::encode($model->chip); ?>
+					</td>
+					<td>
+					</td>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('imei')); ?>:</b>
+						<?php echo CHtml::encode($model->imei); ?>
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('plan')); ?>:</b>
+						<?php echo CHtml::encode($model->plan); ?>
+					</td>
+					<td>
+					</td>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('precioEquipo')); ?>:</b>
+						<?php echo CHtml::encode($model->precioEquipo); ?>
+					</td>
+				</tr>
+				
+				
+				<tr>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('costoRenta')); ?>:</b>
+						<?php echo CHtml::encode($model->costoRenta); ?>
+					</td>
+					<td>
+					</td>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('minutos')); ?>:</b>
+						<?php echo CHtml::encode($model->minutos); ?>
+					</td>
+				</tr>
+				
+				
+				
+				<tr>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('sms')); ?>:</b>
+						<?php echo CHtml::encode($model->sms); ?>
+					</td>
+					<td>
+					</td>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('internet')); ?>:</b>
+						<?php echo CHtml::encode($model->internet); ?>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('fechaContratacion')); ?>:</b>
+						<?php echo CHtml::encode($model->fechaContratacion); ?>
+					</td>
+					<td>
+					</td>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('fechaInicial')); ?>:</b>
+						<?php echo CHtml::encode($model->fechaInicial); ?>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<b><?php echo CHtml::encode($model->getAttributeLabel('fechaFinal')); ?>:</b>
+						<?php echo CHtml::encode($model->fechaFinal); ?>
+					</td>
+					<td>
+					</td>
+					<td>
+					
+					</td>
+				</tr>
+				
+			</tbody>
+		</table>
+		
+</div>
+<br/>
+<div>
+	<p>
+	Notas:
+	</p>
+	<ol>
+		<li>
+		El equipo cuenta con un año de garantía contra defectos de fábrica.
+		</li>
+		<li>
+		Ante cualquier daño ocasionado al equipo celular por mal funcionamiento será responsabilidad del usuario cubrir los gastos para que el eequipo esté en óptimas condiciones mientras dure el plazo forzoso de renta.
+		</li>
+		<li>
+		El equipo celular y la línea son propiedad del Hospital La Carlota (podrá el HLC solicitar el equipo al término del contrato forzoso).
+		</li>
+	</ol>
+</div>
+
+<div class="visible-print-block" style="padding-top: 20px; padding-bottom: 30px; width: 100%">
+	<div style="width: 30%; border-top: solid black 1px; display:inline; float: right;">
+		Firma de entregado
+		<br/>
+	</div>
+	<div style="width: 30%; border-top: solid black 1px; diplay: inline; float: left;">
+		Firma de recibido
+		<br/>
+	</div>
+	
+</div>
+<br/>
+
+
 
 <div class="row buttons hidden-print" style="clear:both">
 	<?php echo CHtml::button('Actualizar', array('submit' => array('telefoniaCelular/update','id'=>$model->keyCTC))); ?>
