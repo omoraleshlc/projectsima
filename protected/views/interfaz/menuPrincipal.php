0bbe97d9442b8cbfa89e6d0df111dc8e628834de
Mitzi Moncada <nesmones1591@hotmail.com>
2014-09-15 10:02:05 -0500

    se cambió la etiqueta en dashboard de inicio

--- protected/views/interfaz/menuPrincipal.php
+++ protected/views/interfaz/menuPrincipal.php
@@ -445,7 +445,7 @@ $watermark_options  = array(
                 <div id="1" class="thumbnail" onmouseover="javascript:cambiarBorde('1');" onmouseout="normal('1');">
                   <img height="40" src="./images/iadmin.jpeg" width="40" data-src="holder.js/160x120" alt="">
                   <div class="caption" align="center">
-                    <small>Administrativo</small>                
+                    <small>Soporte</small>                
                     
                   </div>
                 </div>
