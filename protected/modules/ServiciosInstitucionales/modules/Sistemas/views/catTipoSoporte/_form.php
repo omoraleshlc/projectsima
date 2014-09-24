e8c02992a7ff0ef4dbce3c227672f3f22eac7134
Mitzi Moncada <nesmones1591@hotmail.com>
2014-09-24 15:47:48 -0500

    se modificó el orden de los campos para evitar bug en el que se graba el tipoSoporte durante el post que rellena los almacenes según la entidad

--- protected/modules/ServiciosInstitucionales/modules/Sistemas/views/catTipoSoporte/_form.php
+++ protected/modules/ServiciosInstitucionales/modules/Sistemas/views/catTipoSoporte/_form.php
@@ -14,12 +14,6 @@
 	<p class="note">Campos con <span class="required">*</span> son requeridos.</p>
 
 	<?php echo $form->errorSummary($model); ?>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'descripcion'); ?>
-		<?php echo $form->textField($model,'descripcion',array('size'=>60,'maxlength'=>200)); ?>
-		<?php echo $form->error($model,'descripcion'); ?>
-	</div>
 	
 	<div class="row">
 		<?php echo $form->labelEx($model,'entidad'); ?>
@@ -56,6 +50,12 @@
 		);
 		?>
 		<?php echo $form->error($model,'almacen'); ?>
+	</div>
+        
+	<div class="row">
+		<?php echo $form->labelEx($model,'descripcion'); ?>
+		<?php echo $form->textField($model,'descripcion',array('size'=>60,'maxlength'=>200)); ?>
+		<?php echo $form->error($model,'descripcion'); ?>
 	</div>
 
 	<div class="row buttons">
